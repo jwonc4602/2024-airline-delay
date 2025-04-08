88d5fbad2d9897d539257463666bd3797d99afc8
Jiwon Choi <jwon.choi@mail.utoronto.ca>
2025-04-07 22:05:08 -0400

    download data update

---
+++ scripts/00-download_data.R
@@ -0,0 +1,15 @@
+ #### Preamble ####
+# Purpose: Downloads and saves the data from United States Department of Transportation (Bureau of Transportation Statistics)
+# Author: Jiwon Choi
+# Date: 7 April 2025
+# Contact: jwon.choi@mail.utoronto.ca
+# License: MIT
+
+
+#### Download data ####
+# https://www.transtats.bts.gov/OT_Delay/OT_DelayCause1.asp?20=E
+# Select period (my case January, 2024 - December, 2024)
+# Download Raw Data
+
+
+         
